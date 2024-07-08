56efc6651b29de1cd163fd13a3642fcc947cdbd9
ogdendc <ogdendc>
2024-07-08 20:17:05 +0000

    change

--- subfolder/more_functions.py
+++ subfolder/more_functions.py
@@ -5,7 +5,7 @@ these are helper functions
 from itertools import chain
 
 def serious_function():
-    return "Hello.  I'm seriously serious."
+    return "Hello.  I'm seriously serious.  Seriously"
 
 def mellow_function():
     return "Yeah...I'm mellow."
