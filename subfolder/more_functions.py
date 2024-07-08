ac077aebb53934b4ecba6f1149d3b265cb6fd1b7
ogdendc <ogdendc>
2024-07-08 20:13:33 +0000

    change

--- subfolder/more_functions.py
+++ subfolder/more_functions.py
@@ -3,7 +3,7 @@ these are helper functions
 '''
 
 def serious_function():
-    return "Hello.  I'm serious."
+    return "Hello.  I'm seriously serious."
 
 def mellow_function():
     return "Yeah...I'm mellow."
